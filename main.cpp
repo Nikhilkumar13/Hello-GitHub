fcf554b4d2021090a8207e697283e9ef4f144ff8
Nikhil Kumar <nikkr007@gmail.com>
2015-03-15 00:13:36 +0530

    fixed bug

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
 	cout<<"how is life going on"; 
 	cout<<"this line has been added by browser";
 	cout<<"ek aur branch karke dekhte hai m enjooying it "
+	cout<<"Fixed a bugg";
 	return 0;
 }
